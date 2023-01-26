f5181c384fd863932e3848ff0fea8a58d5078653
Gerald Haboc <77391983+colesitecode@users.noreply.github.com>
2023-01-26 14:35:29 +0800

    first and comleted commit

---
+++ resources/views/home/userpage.blade.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+    <title>Caretastick</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+    <link rel="stylesheet" href="home/css/style2.css">
+    <link rel="stylesheet" href="home/css/responsive2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+</head>
+<body>
+
+
+
+    <nav>
+        <div class="nav_container">
+            <div class="nav_container_1">
+                <div class="logo">
+                    <img src="home/image/logo.png" alt="">
+                </div>
+            </div>
+
+            <div class="nav_container_2" style="align-items: center";>
+                <ul class="nav_ul">
+                    <li>ABOUT</li>
+                    <li>FEATURES</li>
+                    <li>WHO USES CARETASTIC</li>
+                    <li>SUPPORT</li>
+                    <li> RESOURCES</li>
+                </ul>
+            </div>
+
+            <div class="nav_container_3">
+                <div class="signin_btn">
+                    SIGN IN <i class="fa-solid fa-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+     <!-- HERO SECTION START -->
+    <section class="hero_section">
+        <div class="hero_container">
+            <div class="col1">
+                <h1 class="title">Your all-in-one <span style="color:#EB2E72; background: rgba(235, 46, 114, 0.26);"> Care Management  </span>  System <br>
+                     with all the features you need
+                </h1>
+
+                <h3 class="desciption">Lorem ipsum dolor sit amet, quot elit in cum, no eligendi voluptarvLorem ipsum dolor sit amet, <br>
+                    quot elit in cum,  Lorem ipsum dolor sit amet, quot elit in cum, no eligendi voluptarvLorem ipsum 
+                </h3>
+            </div>
+
+            <div class="get_started_btn">
+                Get Started
+            </div>
+
+            <div  class="img_dashboard" >
+                <img class="img_dashboard" src="home/image/laptop.png" alt="">
+            </div>
+        </div>
+    </section>
+
+
+    <!-- POPULAR FEATURES -->
+    <!-- style="background-image: url('image/features_background.png');" -->
+    <section class="features_background" >
+        <div class="features_container">
+            <div class="features_title">
+                <h1 class="title">Our most  <span style="color:#EB2E72; background: rgba(235, 46, 114, 0.26);">popular</span> features</h1>
+                <h3 class="desciption">Lorem ipsum dolor sit amet, quot elit in cum, no eligendi voluptarvLorem <br>
+                    ipsum dolor sit amet, quot elit in cum,  Lorem ipsum dolor sit amet, quot elit <br>
+                     in cum, no eligendi voluptarvLorem ipsum </h3>
+            </div>
+
+
+            <div class="features">
+                <div class="col4">
+                    <img src="home/image/feature1.png" alt="">
+                    <h1 class="features_title">Feature 1</h1>
+                    <h4 class="features_description">Lorem ipsum dolor sit amet, quot elit in cum, no eligendi voluptarvLorem </h4>
+                </div>
+
+
+                <div class="col4 features_padding">
+                    <img src="home/image/feature2.png" alt="">
+                    <h1 class="features_title">Feature 1</h1>
+                    <h4 class="features_description">Lorem ipsum dolor sit amet, quot elit in cum, no eligendi voluptarvLorem </h4>
+                </div>
+
+                <div class="col4">
+                    <img src="home/image/feature3.png" alt="">
+                    <h1 class="features_title">Feature 1</h1>
+                    <h4 class="features_description">Lorem ipsum dolor sit amet, quot elit in cum, no eligendi voluptarvLorem </h4>
+                </div>
+
+                <div class="col4 features_padding">
+                    <img src="home/image/feature4.png" alt="">
+                    <h1 class="features_title">Feature 1</h1>
+                    <h4 class="features_description">Lorem ipsum dolor sit amet, quot elit in cum, no eligendi voluptarvLorem </h4>
+                </div>
+
+            </div>
+
+                <div class="view_more_btn">
+                    VIEW MORE
+                </div>
+        </div>
+    </section>
+
+
+
+    <!-- Designed Features -->
+    <section>
+        <div class="Designed_Container">
+        <div class="designed_title">
+            <h1 class="title">Designed Features To track <br> <span style="color:#EB2E72; background: rgba(235, 46, 114, 0.26);">Easily!</span></h1>
+            <h3 class="desciption">Lorem ipsum dolor sit amet, quot elit in cum, no eligendi voluptarvLorem <br>
+                ipsum dolor sit amet, quot elit in cum,  Lorem ipsum dolor sit amet, quot elit <br>
+                 in cum, no eligendi voluptarvLorem ipsum </h3>
+        </div>
+        <div>
+            <img class="img_design" src="home/image/designd_laptop.png" alt="">
+        </div>
+
+        <div class="view_more_btn2">
+            VIEW MORE
+        </div>
+        </div>
+    </section>
+
+    <!-- GETTING STARTED -->
+    <section>
+        <div class="getting_started_container">
+            <div class="designed_title">
+                <h1 class="title">Getting Started Is Easy!</h1>
+            </div>
+            <div class="steps_img">
+                <img class="img_steps" src="home/image/steps1.png" alt="">
+            </div>
+            <div class="getting_started_btn">
+                VIEW MORE
+            </div>
+        </div>
+    </section>
+
+
+    <!-- TESTIMONIAL -->
+    <section class="testimonial">
+        <div class="testimonial_title">
+            <h1 class="title">Clients Testimonial</h1>
+        </div>
+        <div class="testimonial_container">
+        <div class="col3">
+
+            <img src="home/image/star.png" alt="">
+
+            <h3>
+                Lorem ipsum dolor sit amet, quot elit in  Lorem ipsum dolor sit amet, quot elit in  
+                Lorem ipsum dolor sit amet, quot elit in  Lorem ipsum dolor sit amet, quot elit in  
+                Lorem ipsum dolor sit amet, quot elit in  Lorem ipsum dolor sit amet, quot elit in   
+            </h3>
+
+            <h1>
+                Albert Flores
+            </h1>
+
+            <h3>UX Designer</h3>
+
+        </div>
+
+        <div class="col3">
+
+            <img src="home/image/star.png" alt="">
+
+            <h3>
+                Lorem ipsum dolor sit amet, quot elit in  Lorem ipsum dolor sit amet, quot elit in  
+                Lorem ipsum dolor sit amet, quot elit in  Lorem ipsum dolor sit amet, quot elit in  
+                Lorem ipsum dolor sit amet, quot elit in  Lorem ipsum dolor sit amet, quot elit in   
+            </h3>
+
+            <h1>
+                Albert Flores
+            </h1>
+
+            <h3>UX Designer</h3>
+
+        </div>
+
+        <div class="col3">
+
+            <img src="home/image/star.png" alt="">
+
+            <h3>
+                Lorem ipsum dolor sit amet, quot elit in  Lorem ipsum dolor sit amet, quot elit in  
+                Lorem ipsum dolor sit amet, quot elit in  Lorem ipsum dolor sit amet, quot elit in  
+                Lorem ipsum dolor sit amet, quot elit in  Lorem ipsum dolor sit amet, quot elit in   
+            </h3>
+
+            <h1>
+                Albert Flores
+            </h1>
+
+            <h3>UX Designer</h3>
+
+        </div>
+        </div>
+    </section>
+
+
+    <!-- GALLERY -->
+    <section class="gallery">
+        <div class="testimonial_title">
+            <h1 class="title" style="margin-bottom: 50px;">Gallery</h1>
+        </div>
+
+        <div class="gallery_container">
+
+            <div class="col1">
+                <div class="col1_img"><img style="padding: 10px;" src="home/image/gal1.png" alt=""></div>
+                <div class="col1_img"><img style="padding: 10px;" src="home/image/gal2.png" alt=""></div>
+                <div class="col1_img"><img style="padding: 10px;" src="home/image/gal3.png" alt=""></div>
+            </div>
+
+            <div class="col2">
+                <div class="col1_img"><img style="padding: 10px;" src="home/image/gal4.png" alt=""></div>
+                <div class="col2_img"><img style="padding: 10px;" src="home/image/gal5.png" alt=""></div>
+            </div>
+
+        </div>
+    </section>
+
+
+    <!-- CTA -->
+    <section class="cta">   
+        <div class="cta_container">
+            <div class="col1">
+                <div>
+                    <h1 class="cta_title"> Book Your Appointment</h1>
+                </div>
+                <div class="getting_started_btn">
+                    BOOK NOW
+                </div>
+            </div>
+            {{-- <div class="col2">
+                <img class="img_cta" src="home/image/dashboard.png" alt="">
+            </div> --}}
+        </div>
+    </section>
+
+    <!-- FOOTER -->
+    <section class="footer_Background">
+        <div class="footer_container">
+        <div class="email_subscription">
+            <div class="text_box">
+                <input class="email_subscription_text_box" type="text" name="email" placeholder="Enter E-mail Address" required>
+            </div>
+            <div class="subscription_btn">
+                SUBSCRIBE
+            </div>
+        </div>
+
+        <div class="footer_details">
+            <div class="col4">
+                <img src="home/image/logo.png" alt="">
+                <h3 class="footer_desciption">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </h3>
+
+                <div class="social_media">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-square-instagram"></i>
+                    <i class="fa-brands fa-youtube"></i>
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </div>
+            </div>
+
+            <div class="col4">
+                <h1 class="footer_title">Company</h1>
+                <h3 class="link">About us</h3>
+                <h3 class="link">Letest Events</h3>
+                <h3 class="link">Letest Events</h3>
+            </div>
+
+            <div class="col4">
+                <h1 class="footer_title">Day Care</h1>
+                <h3 class="link">Child</h3>
+                <h3 class="link">Adult</h3>
+                <h3 class="link">Elder</h3>
+            </div>
+
+            <div class="col4">
+                <h1 class="footer_title">Contact</h1>
+                <h3 class="link">Nakshi Homes (4th Fl.),
+                    6/1A Segunbagicha, Dhaka-1000, Bangladesh</h3>
+                <h3 class="link">support@xyz.com</h3>
+                <h3 class="link">+88 019##########</h3>
+            </div>
+        </div>
+
+        </div>
+    </section>
+
+    <section>
+        <div class="copyright">
+            <h3 class="link">Copyright © 2018. All Rights Reserved.</h3>
+        </div>
+    </section>
+    
+</body>
+</html>
